7f0cb11e66a64524b090a1f4d22d49993befc2c6
Mateskoda <skodamate93@gmail.com>
2020-10-13 15:35:24 +0200

    first

---
+++ src/file.java
@@ -0,0 +1,9 @@
+public class file {
+    public static void main(String[] args) {
+        int[] array1 = new int[20];
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = (int) (Math.random() * 101);
+        }
+
+    }
+}
